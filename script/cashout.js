d6f9b9789f6acf58d2efb3cbf258dbbde886eaaa
ruhul2003 <ruhul941020@gmail.com>
2026-02-20 00:33:46 +0600

    Balance part functionality added

--- script/cashout.js
+++ script/cashout.js
@@ -5,4 +5,18 @@ document.getElementById("cashout-btn").addEventListener("click", function () {
     const cashoutAmountInput = document.getElementById("cashout-amount");
     const cashoutAmount = cashoutAmountInput.value;
     console.log(cashoutAmount);
+
+
+    const balanceElement = document.getElementById("balance");
+    const balance = balanceElement.innerText;
+    console.log(balance);
+
+
+    const newBalance = Number(balance) - Number(cashoutAmount);
+    balanceElement.innerText = newBalance;
+    console.log("New balance is",newBalance);
+
+    
+
+    
 });
