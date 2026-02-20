ecdc2071d85e0bf88815cc844ab02ef52341d881
ruhul2003 <ruhul941020@gmail.com>
2026-02-21 01:12:11 +0600

    Add money activities done

---
+++ script/addMoney.js
@@ -0,0 +1,38 @@
+document.getElementById("add-money-btn").addEventListener("click", function () {
+
+    const bankAccount = getValueFromInput("add-money-bank");
+    console.log(bankAccount);
+    if(bankAccount == "Select Bank"){
+        alert("Please Select a Bank");
+        return;
+    }
+
+    // get account number
+
+    const accountNumber =getValueFromInput("add-money-number");
+    if(accountNumber.length != 11){
+        alert("Invalid Account Number");
+        return;
+    }
+
+    // get amount
+
+    const amount = getValueFromInput("add-money-amount");
+    if(amount.length < 1){
+        alert("Invalid Amount");
+        return;
+    }
+
+    const pin = getValueFromInput("add-money-pin");
+    if(pin.length != 4){
+        alert("Invalid Pin");
+        return;
+    }else{
+        alert(`Add Money Successfull from ${bankAccount} bank to ${accountNumber} at ${new Date().toLocaleString()}`);
+    }
+
+    const currentBalance = getBalance();
+    const newBalance = currentBalance + Number(amount);
+    console.log(newBalance);
+    setBalance(newBalance);
+});
